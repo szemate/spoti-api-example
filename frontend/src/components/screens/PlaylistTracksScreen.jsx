072da80d9bd10a90f27f08607954da10fe3df0e8
Máté Szendrő <szemate@posteo.net>
2023-11-23 18:19:38 +0100

    Add example for navigation if access token is not set

--- frontend/src/components/screens/PlaylistTracksScreen.jsx
+++ frontend/src/components/screens/PlaylistTracksScreen.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 
 // import PlaylistTracksFilterModal from "../dialogs/PlaylistTracksFilterModal";
 
 function PlaylistTracksScreen() {
+  const navigate = useNavigate();
+
+  if (!localStorage.getItem("access_token")) {
+    return navigate("/");
+  }
     
   return (
     <div className=" bg-orange-200 grow">
